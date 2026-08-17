369efb20c6bfe61df2c2119d00bc461a5de4c983
Prashant Gautam <prashantgautam8077@gmail.com>
2026-08-17 21:16:34 +0530

    Time: 567 ms (14.98%), Space: 296.4 MB (22.06%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    int rec(int level, int last_taken, vector<int>& nums, vector<vector<int>>& dp){
-        int n = nums.size();
-        //base case
-        if(level==n){
+
+    vector<vector<int>> dp;
+
+    int rec(vector<int>& nums, int level, int prev) {
+        // base case
+        if (level == nums.size())
             return 0;
+
+        // cache check
+        if (dp[level][prev+1] != -1)
+            return dp[level][prev+1];
+
+        int ans = rec(nums, level + 1, prev);
+
+
+
+        // compute
+
+        // for (int prev_taken = 0; prev_taken < level; prev_taken++) {
+        //     if (arr[prev_taken] < arr[level]) {
+        //         ans = max(ans, 1 + rec(prev_taken));
+        //     }
+        // }
+
+        if (prev == -1 || nums[level] > nums[prev]) {
+            ans = max(ans, 1 + rec(nums, level + 1, level));
         }
-
-        // take if last_taken < x[level]
-        // don't take it last_taken >= x[level]
-
-        //already calculated
-        if(dp[level][last_taken+1]!=-1){
-            return dp[level][last_taken+1];
-        }
-
-        int notTake = rec(level + 1, last_taken, nums,dp);
-
-        int take = 0;
-        if(last_taken==-1 || nums[level]>nums[last_taken]){
-            take = 1 + rec(level+1,level,nums,dp);
-        } 
-        return dp[level][last_taken+1] = max(take,notTake);
+        // save and return
+        return dp[level][prev + 1] = ans;
     }
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return rec(0,-1,nums,dp);
-        
+
+        // prev can be -1, so we use prev + 1 as the index
+        dp.assign(n, vector<int>(n + 1, -1));
+
+        return rec(nums, 0, -1);
     }
 };
